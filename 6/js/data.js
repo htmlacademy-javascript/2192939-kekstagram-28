e5b7a7fb1fdbbbdd21ed930bd9e51104689e8ef0
Keks <keks@htmlacademy.ru>
2023-03-19 13:27:09 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/data.js
@@ -0,0 +1,25 @@
+import { idGenerator, idCommentGenerator, createRandomElement, getRandomInteger } from './utils.js';
+import { MESSAGES, NAMES, DESCRIPTIONS, MIN_COMMENTS_QUANTITY, MAX_COMMENTS_QUANTITY, MAX_PHOTOS_QUANTITY } from './constants.js';
+
+
+const createComments = () => ({
+  id: idCommentGenerator(),
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: createRandomElement(MESSAGES),
+  name: createRandomElement(NAMES),
+});
+
+const createPhoto = () => {
+  const id = idGenerator();
+  const quantityComments = getRandomInteger(MIN_COMMENTS_QUANTITY, MAX_COMMENTS_QUANTITY);
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: createRandomElement(DESCRIPTIONS),
+    likes: getRandomInteger(15, 200),
+    comments: Array.from({ length: quantityComments }, createComments),
+  };
+};
+
+
+export const photos = () => Array.from({ length: MAX_PHOTOS_QUANTITY }, createPhoto);
---
+++ 6/js/utils.js
@@ -0,0 +1,32 @@
+const generateId = () => {
+  let lastId = 0;
+
+  return function () {
+    lastId += 1;
+    return lastId;
+  };
+};
+
+const generateRandomId = (min, max, getRandom) => {
+  const previosValues = [];
+  return function () {
+    let currentValue = getRandom(min, max);
+    while (previosValues.includes(currentValue)) {
+      currentValue = getRandom(min, max);
+    }
+    previosValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+export const getRandomInteger = (min, max) => {
+  const rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+};
+
+export const idGenerator = generateId();
+export const idCommentGenerator = generateRandomId(1, 500, getRandomInteger);
+
+export const createRandomElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+export const isEscapeKey = (evt) => evt.key === 'Escape';
